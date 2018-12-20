cf9974e32e3b94b61c166f02f44e08c4f3bc7890
m-aleks <al.mar@ukr.net>
2018-12-20 10:59:34 +0200

    first lesson task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Forrest Gump",
+      year: "1994",
+      genre: "comedy-drama",
+      actors: ["Tom Hanks", "Robin Wright", "Gary Sinise"]
+    }
+    //this.output = this.output.bind(this);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <header className="app_header">
+            <h1>
+              My React App
+            </h1>
+          </header>
+          <section>
+            <div className="section_heading">
+              <h2>
+                {this.state.title}
+              </h2>
+            </div>
+            <div className="section_content">
+              <div className="summary">
+                <div className="summary_item">
+                  <h4 className="summary_item_heading inline">Year:</h4>
+                  <span className="summary_item_text">{this.state.year}</span>
+                </div>
+                <div className="summary_item">
+                  <h4 className="summary_item_heading inline">Genre:</h4>
+                  <span className="summary_item_text">{this.state.genre}</span>
+                </div>
+                <div className="summary_item">
+                  <h4 className="summary_item_heading inline">Actors:</h4>
+                  <span className="summary_item_text">{this.state.actors.join(", ")}</span>
+                </div>
+              </div>
+
+            </div>
+
+          </section>
+        </div>
       </div>
     );
   }
